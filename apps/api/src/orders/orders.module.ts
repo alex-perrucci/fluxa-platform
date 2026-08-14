eb585164d4b5fb07a1f2f4f03af426fb5152f684
alex-perrucci <128584526+alex-perrucci@users.noreply.github.com>
2026-08-14 17:52:48 +0200

    register manual sale order endpoint

--- apps/api/src/orders/orders.module.ts
+++ apps/api/src/orders/orders.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ManualOrderItemsController } from './manual-order-items.controller';
+import { ManualOrderItemsService } from './manual-order-items.service';
 import { OrderAccessService } from './order-access.service';
 import { OrderPricingService } from './order-pricing.service';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 
 @Module({
-  controllers: [OrdersController],
-  providers: [OrdersService, OrderAccessService, OrderPricingService],
+  controllers: [OrdersController, ManualOrderItemsController],
+  providers: [
+    OrdersService,
+    ManualOrderItemsService,
+    OrderAccessService,
+    OrderPricingService,
+  ],
   exports: [OrdersService],
 })
 export class OrdersModule {}
