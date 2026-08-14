7bc21c31239c96e0ba227929f4958c91fb9c6274
alex-perrucci <128584526+alex-perrucci@users.noreply.github.com>
2026-08-14 15:12:17 +0200

    feat(windows): add fiscal receipt raw printing channel

---
+++ apps/pos/windows/runner/fiscal_receipt_printing_channel_win32.h
@@ -0,0 +1,8 @@
+#ifndef RUNNER_FISCAL_RECEIPT_PRINTING_CHANNEL_WIN32_H_
+#define RUNNER_FISCAL_RECEIPT_PRINTING_CHANNEL_WIN32_H_
+
+#include <flutter/binary_messenger.h>
+
+void RegisterFiscalReceiptPrintingChannel(flutter::BinaryMessenger* messenger);
+
+#endif  // RUNNER_FISCAL_RECEIPT_PRINTING_CHANNEL_WIN32_H_
