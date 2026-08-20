abc619e84ac6bd8a8f3bc5b66e4ee4e457dce718
alex-perrucci <128584526+alex-perrucci@users.noreply.github.com>
2026-08-20 10:47:06 +0200

    feat(ade): wire CIE push auth services

--- apps/ade-fiscal-worker/src/ade-fiscal-worker.module.ts
+++ apps/ade-fiscal-worker/src/ade-fiscal-worker.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LoggerModule } from 'nestjs-pino';
+import { AdeAuthController } from './ade-auth.controller';
+import { AdeAuthProfileService } from './ade-auth-profile.service';
+import { AdeAuthService } from './ade-auth.service';
 import { AdeBrowserService } from './ade-browser.service';
+import { AdeCieCredentialsService } from './ade-cie-credentials.service';
 import { AdeDryRunController } from './ade-dry-run.controller';
 import { AdeDryRunService } from './ade-dry-run.service';
 import { AdeHealthController } from './ade-health.controller';
@@ -22,14 +26,17 @@ import { AdeWebFiscalService } from './ade-web-fiscal.service';
       },
     }),
   ],
-  controllers: [AdeHealthController, AdeDryRunController],
+  controllers: [AdeHealthController, AdeDryRunController, AdeAuthController],
   providers: [
     AdeRuntimeConfigService,
     AdeBrowserService,
     AdeSessionService,
     AdeSelectorProfileService,
+    AdeAuthProfileService,
+    AdeCieCredentialsService,
     AdeInternalAuthGuard,
     AdeDryRunService,
+    AdeAuthService,
     AdeWebFiscalService,
   ],
 })
