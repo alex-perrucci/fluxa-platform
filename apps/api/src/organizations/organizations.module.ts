31ae00acb4168029c4298eafddd3ec006c2c55a4
alex-perrucci <128584526+alex-perrucci@users.noreply.github.com>
2026-08-27 11:59:52 +0200

    feat(organizations): register atomic provisioning service

--- apps/api/src/organizations/organizations.module.ts
+++ apps/api/src/organizations/organizations.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { OrganizationProvisioningService } from './organization-provisioning.service';
 import { OrganizationsController } from './organizations.controller';
 import { OrganizationsService } from './organizations.service';
 
 @Module({
   controllers: [OrganizationsController],
-  providers: [OrganizationsService],
+  providers: [OrganizationsService, OrganizationProvisioningService],
 })
 export class OrganizationsModule {}
