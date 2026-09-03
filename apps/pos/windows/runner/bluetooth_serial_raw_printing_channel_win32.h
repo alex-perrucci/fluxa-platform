1330d0c8dd366474ae4ea4ea07dcacb6553dc70c
alex-perrucci <128584526+alex-perrucci@users.noreply.github.com>
2026-09-03 17:02:51 +0200

    feat(pos): add Windows Bluetooth serial raw printing channel

---
+++ apps/pos/windows/runner/bluetooth_serial_raw_printing_channel_win32.h
@@ -0,0 +1,9 @@
+#ifndef RUNNER_BLUETOOTH_SERIAL_RAW_PRINTING_CHANNEL_WIN32_H_
+#define RUNNER_BLUETOOTH_SERIAL_RAW_PRINTING_CHANNEL_WIN32_H_
+
+#include <flutter/binary_messenger.h>
+
+void RegisterBluetoothSerialRawPrintingChannel(
+    flutter::BinaryMessenger* messenger);
+
+#endif  // RUNNER_BLUETOOTH_SERIAL_RAW_PRINTING_CHANNEL_WIN32_H_
